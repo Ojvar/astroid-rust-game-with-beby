4598505529b1696053d2b8e55068a6ad8ab12962
Ojvar <MOjvar.AmirHossein@Gmail.com>
2023-12-21 14:09:09 +0330

    prepare spaceship

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::*;
+
+mod camera;
+mod debug;
+mod movement;
+mod spaceship;
+
+use camera::CameraPlugin;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
+fn main() {
+    App::new()
+        // Bevy plugins
+        .insert_resource(ClearColor(Color::rgb(0.1, 0., 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
+        .add_plugins(DefaultPlugins)
+        // Use plugins
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
+        .run();
+}
